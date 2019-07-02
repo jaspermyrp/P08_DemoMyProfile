971f4853d7f619335115046d9affffcb29aaf3c7
Jasper <17020202@myrp.edu.sg>
2019-07-02 11:28:50 +0800

    Commit

--- app/src/main/java/sg/edu/rp/c346/demomyprofile/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/demomyprofile/MainActivity.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etName, etGpa;
+    RadioGroup rgGender;
+    Button btnSave;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,15 @@ public class MainActivity extends AppCompatActivity {
 
         etName = findViewById(R.id.etName);
         etGpa = findViewById(R.id.etGpa);
+        rgGender = findViewById(R.id.rgGenders);
+        btnSave = findViewById(R.id.btnSave);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                savePreference();
+            }
+        });
     }
 
     @Override
@@ -27,6 +43,13 @@ public class MainActivity extends AppCompatActivity {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String strName = prefs.getString("strName", "");
         float fltGpa = prefs.getFloat("fltGpa", 0);
+        String strGender = prefs.getString("strGender", "Male");
+
+        if(strGender.equals("Male")){
+            rgGender.check(R.id.rbMale);
+        }else{
+            rgGender.check(R.id.rbFemale);
+        }
 
         etName.setText(strName);
         etGpa.setText(fltGpa+"");
@@ -35,15 +58,25 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+        savePreference();
 
-        // Get user inputs //
+    }
+
+    private void savePreference(){
         String stringName = etName.getText().toString();
         float floatGpa = Float.parseFloat(etGpa.getText().toString());
+
+        String gender = "";
+        if(rgGender.getCheckedRadioButtonId() == R.id.rbMale){
+            gender = "Male";
+        }else{
+            gender = "Female";
+        }
 
         // Save user inputs //
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         SharedPreferences.Editor prefsEdit = prefs.edit();
-        prefsEdit.putString("strName", stringName).putFloat("fltGpa",floatGpa);
+        prefsEdit.putString("strName", stringName).putFloat("fltGpa",floatGpa).putString("strGender",gender);
         prefsEdit.commit();
     }
 
